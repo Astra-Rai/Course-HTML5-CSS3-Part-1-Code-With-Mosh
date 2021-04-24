860c4ea9f69283613a2afc0c689bd546488ba83c
Astra Rai <42603584+Astra-Rai@users.noreply.github.com>
2021-04-24 02:26:18 -0400

    obj literals: members, methods, properties

---
+++ objects-2-object-literals-part-1.js
@@ -0,0 +1,20 @@
+const circle = {
+  radius: 1,
+  locaton: {
+    x: 1,
+    y: 1
+  },
+
+  draw: function() {
+    console.log("draw");
+  }
+};
+
+/*
+    - members object: radius, location, and draw function
+    - properties: used to hold values, radius and location are examples of properties
+    - functions and methods are used to define logic
+    - draw is method of the object ' circle '
+    - obj literal, syntax, members, mehtods, properties
+
+*/
